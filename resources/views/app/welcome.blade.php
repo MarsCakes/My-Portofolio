f2541cf96bb7a8667adf1f0ab4a28b091ea05ae0
MarsCakes <imanuel.peter2005@gmail.com>
2025-09-28 23:55:34 +0700

    added portofolio section

--- resources/views/app/welcome.blade.php
+++ resources/views/app/welcome.blade.php
@@ -1,11 +1,39 @@
 @extends('layout.header')
 
-@section('content')
-    <div class="container mt-4 align-center">
-        <div class="align-center">
+@section('introduction')
+    <div class="container mt-4 fluid text-center">
+        <div class="align-items-center">
             <h1>Welcome to My Portofolio!</h1>
-            <p>Hello! My name is Imanuel Peter Phang, I am a Software Engineering student of Bina Nusantara University and
+            <img src="{{asset('images/Soukaku.webp')}}" alt="myPicture" class="img-fluid-rounded" style="max-width: 300px;">
+            <p class="mt-4">Hello! My name is Imanuel Peter Phang, I am a Software Engineering student of Bina Nusantara
+                University and
                 this is my portofolio page!</p>
         </div>
     </div>
+@endsection
+
+@section('portofolio')
+    <h2 class="text-center">My Portofolio</h2>
+    <div class="container mt-8">
+        <div class="row">
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="{{asset('images/Projects/Asphatl.png')}}" class="card-img-top" alt="Project 1">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Asphatl 9</h5>
+                        <p class="card-text text-center">Human Computer Interaction Project</p>
+                        <p class="card-text text-center">Frontend and Backend Developer</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="{{asset('images/Projects/image.png')}}" class="card-img-top" alt="Project 2">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Ruang Murid</h5>
+                        <p class="card-text text-center">Software Engineering Project</p>
+                        <p class="card-text text-center">Frontend Developer</p>
+                    </div>
+                </div>
+            </div>
 @endsection
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,22 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title','Website Pribadi')</title>
+    <title>@yield('title', 'Website Pribadi')</title>
     <link href="{{ asset('css/bootstrap.min.css')}}" rel="stylesheet">
 </head>
+
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="/">MyWebsite</a>
-        <button class="navbar-toggler" type="button" 
-        data-bs-toggle="collapse" 
-        data-bs-target="#navbarNav" 
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle Navigation">
-        <span class="navbar-toggler-icon"></span>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle Navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id=navbarNav>
             <ul class="navbar-nav">
@@ -29,9 +27,11 @@
             </ul>
         </div>
     </nav>
+    @yield('introduction')
     <main>
-        @yield('content')
+        @yield('portofolio')
     </main>
-    <script src = "{{ asset('js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js')}}"></script>
 </body>
+
 </html>
