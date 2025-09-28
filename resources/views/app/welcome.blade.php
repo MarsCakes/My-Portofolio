0cce02b2bee3c6d7e227f9a1a87adc6b104b8fb9
MarsCakes <imanuel.peter2005@gmail.com>
2025-09-28 22:05:46 +0700

    first branch commit

--- resources/views/app/welcome.blade.php
+++ resources/views/app/welcome.blade.php
@@ -4,7 +4,8 @@
     <div class="container mt-4 align-center">
         <div class="align-center">
             <h1>Welcome to My Portofolio!</h1>
-            <p>This is the welcome page.</p>
+            <p>Hello! My name is Imanuel Peter Phang, I am a Software Engineering student of Bina Nusantara University and
+                this is my portofolio page!</p>
         </div>
     </div>
 @endsection
