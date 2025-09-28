da833be79d67d0f87598b679d62addf28bc96482
MarsCakes <imanuel.peter2005@gmail.com>
2025-09-28 21:53:42 +0700

    First commit

---
+++ resources/views/app/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.header')
+
+@section('content')
+    <div class="container mt-4 align-center">
+        <div class="align-center">
+            <h1>Welcome to My Portofolio!</h1>
+            <p>This is the welcome page.</p>
+        </div>
+    </div>
+@endsection
